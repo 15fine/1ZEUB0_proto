b822c3eb0ed6fddb8cd407317c40b1af3b34d34e
Dillon Gonsalves <dgonsalves999@gmail.com>
2021-12-23 23:06:10 +0400

    added navbar and results HTML, removed list

--- app.py
+++ app.py
@@ -2,19 +2,15 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+# main home page
 @app.route('/')
 def index():
     return render_template("home.html")
 
-# display page with checklist of ingredients
-@app.route('/list')
-def list():
-    return False
-
 # function to display results based on selection of ingredients
 @app.route('/results')
 def result():
-    return True
+    return render_template("results.html")
 
 if __name__ == "__main__":
     app.run()
