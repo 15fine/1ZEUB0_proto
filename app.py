c09e510ebeb064aa6c44fbbc279f35e9b5bb8c5e
Dillon Gonsalves <dgonsalves999@gmail.com>
2022-01-05 12:59:11 +0400

    Added recipes and basic result redirection

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
+import time
 
 app = Flask(__name__)
 
@@ -9,9 +10,36 @@ def index():
         ingredients = request.form.getlist('ingredient')
         occasion = request.form.getlist('occasion')
         diet = request.form.getlist('diet')
-        return "Done"
+        print(ingredients)
+        if ingredients == []:
+            return render_template("all.html")
+        for i in range(len(ingredients)):
+            if ingredients[i] == "1":
+                return render_template("chicken.html")
+            elif ingredients[i] == "3":
+                return render_template("salmon.html")
+            elif ingredients[i] == "2":
+                return render_template("egg.html")
+            elif ingredients[i] == "4":
+                return render_template("carrot.html")
     else:
         return render_template("home.html")
+
+# results
+@app.route('/results', methods=["POST", "GET"])
+def results(resultid):
+    if request.method == "GET":
+        if resultid == 1:
+            return "Chicken"
+        elif resultid == 3:
+            return "Salmon"
+        elif resultid == 2:
+            return "Egg"
+        elif resultid == 4:
+            return "Carrots"
+    
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run()
